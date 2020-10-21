5fb837912ae8c416b3db0d6e9f9de62eec21aca8
100pecheneK <mistermihail23@gmail.com>
2020-10-21 21:30:17 +0500

    Fix: Анимация перехода

--- containers/MainPage.js
+++ containers/MainPage.js
@@ -2,13 +2,36 @@ import {AnimatePresence, motion} from 'framer-motion'
 import React from 'react'
 
 
+export const variants = {
+  enter: {
+    x: -1000,
+    opacity: 0
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1
+  },
+  exit: {
+    zIndex: 0,
+    x: 1000,
+    opacity: 0
+  }
+}
+
 export default function MainPage({children, pageKey}) {
   return (
     <AnimatePresence>
       <motion.div
         key={pageKey}
-        initial={{x: '-100%'}}
-        animate={{x: '0%', transition: '1s'}}
+        variants={variants}
+        initial="enter"
+        animate="center"
+        exit="exit"
+        transition={{
+          x: {type: 'spring', stiffness: 300, damping: 30},
+          opacity: {duration: 0.2}
+        }}
       >
         {children}
       </motion.div>
