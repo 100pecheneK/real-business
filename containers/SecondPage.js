fc688e927643513b361fee722bee90cbfe04febb
100pecheneK <mistermihail23@gmail.com>
2020-11-12 11:28:34 +0500

    Пофиксил баг с мобильной версией анимации перехода по страницам

--- containers/SecondPage.js
+++ containers/SecondPage.js
@@ -1,19 +1,18 @@
-import {AnimatePresence, motion} from 'framer-motion'
-import {variants} from './MainPage'
+import { AnimatePresence, motion } from 'framer-motion'
+import { variants } from './MainPage'
 
-
-export default function SecondPage({children, pageKey}) {
+export default function SecondPage({ children, pageKey }) {
   return (
     <AnimatePresence>
       <motion.div
         key={pageKey}
         variants={variants}
-        initial="exit"
-        animate="center"
-        exit="enter"
+        initial='exit'
+        animate='center'
+        exit='enter'
         transition={{
-          x: {type: 'spring', stiffness: 300, damping: 30},
-          opacity: {duration: 0.2}
+          x: { type: 'spring', stiffness: 300, damping: 30 },
+          opacity: { duration: 0.2 },
         }}
       >
         {children}
