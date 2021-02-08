989650303e407b6396c7a48c20291fdf70bf3c65
100pecheneK <mistermihail23@gmail.com>
2021-02-08 21:38:26 +0500

    fix number imput: , and . now not read

--- components/Input.js
+++ components/Input.js
@@ -1,8 +1,14 @@
 import styles from '../styles/Home.module.css'
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 
-
-export function Input({type = 'text', name, placeholder, label, onChange, value = ''}) {
+export function Input({
+  type = 'text',
+  name,
+  placeholder,
+  label,
+  onChange,
+  value = '',
+}) {
   const [_value, _setValue] = useState('')
 
   useEffect(() => {
@@ -11,23 +17,36 @@ export function Input({type = 'text', name, placeholder, label, onChange, value 
     }
   }, [value])
 
-  const _onChange = (e) => {
+  const _onKeyDown = e => {
+    if (e.key === '.' || e.key === ',') {
+      e.preventDefault()
+    }
+  }
+  const _onChange = e => {
     _setValue(e.target.value)
     onChange(e)
   }
 
   return (
     <div className={styles['input-container']}>
-      <label className={styles.label} htmlFor={name}>{label}</label>
-      <input type={type} name={name} value={_value} onChange={_onChange}
-             placeholder={placeholder}
-             className={styles.input}/>
+      <label className={styles.label} htmlFor={name}>
+        {label}
+      </label>
+      <input
+        type={type}
+        name={name}
+        value={_value}
+        onKeyDown={_onKeyDown}
+        onChange={_onChange}
+        placeholder={placeholder}
+        className={styles.input}
+      />
     </div>
   )
 }
 export function NumberInput({ onChange, ...props }) {
-  const _onChange = (e) => {
-    onChange({ name: e.target.name, value: +e.target.value });
-  };
-  return <Input {...props} onChange={_onChange} type="number" />;
+  const _onChange = e => {
+    onChange({ name: e.target.name, value: +e.target.value })
+  }
+  return <Input {...props} onChange={_onChange} type='number' />
 }
